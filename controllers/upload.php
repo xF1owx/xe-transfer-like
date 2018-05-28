61a9ff26b6e2f31f6109edda01009bee4177e046
xF1owx <florian.r@codeur.online>
2018-05-28 14:53:10 +0200

    données form > PHP

--- controllers/upload.php
+++ controllers/upload.php
@@ -2,7 +2,19 @@
 
 require_once('views/home.html');
 
+
+$usermail = ($_POST['userMail']);
+$destinataireMail = ($_POST['destinataireMail']);
+$message = ($_POST['message']);
+
+var_dump ($usermail);
+echo "</br>";
+var_dump ($destinataireMail);
+echo "</br>";
+var_dump ($message); 
+echo "</br>";
 var_dump($_FILES['upFile']);
+echo "</br>";
 
 
 if (isset($_FILES['upFile'])){
@@ -11,8 +23,8 @@ if (isset($_FILES['upFile'])){
     $path = $_SERVER['DOCUMENT_ROOT'].'/xe-transfer-like/assets/medias/files/'.$_FILES['upFile']['name'];
     move_uploaded_file($_FILES['upFile']['tmp_name'], $path);
 
-  
+  uploadFile();
 
 }
 
-var_dump()
+
