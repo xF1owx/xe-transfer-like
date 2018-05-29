4ec181747a9078243d80e4c462f759c73d88aef0
xF1owx <florian.r@codeur.online>
2018-05-29 09:12:38 +0200

    ajout fonction pour inscription Bdd transfert de variables a regler ..

--- controllers/upload.php
+++ controllers/upload.php
@@ -1,30 +1,34 @@
 <?php 
 
 require_once('views/home.html');
+require_once('models/request.php');
 
 
 $usermail = ($_POST['userMail']);
-$destinataireMail = ($_POST['destinataireMail']);
+$destinatairemail = ($_POST['destinataireMail']);
 $message = ($_POST['message']);
+$date = time();
+$filename = $_FILES['upFile']['name'];
 
-var_dump ($usermail);
-echo "</br>";
-var_dump ($destinataireMail);
-echo "</br>";
-var_dump ($message); 
-echo "</br>";
-var_dump($_FILES['upFile']);
-echo "</br>";
 
 
 if (isset($_FILES['upFile'])){
+
     
 
-    $path = $_SERVER['DOCUMENT_ROOT'].'/xe-transfer-like/assets/medias/files/'.$_FILES['upFile']['name'];
+    $path = $_SERVER['DOCUMENT_ROOT'].'/xe-transfer-like/assets/medias/files/'.$filename;
     move_uploaded_file($_FILES['upFile']['tmp_name'], $path);
 
-  uploadFile();
+  uploadFileuser($usermail);
+  uploadFileFile($path,$date,$filename);
+  uploadFileDest($destinatairemail);
 
 }
+else{
+    echo "error";
+}
+
+
+
 
 
--- models/request.php
+++ models/request.php
@@ -1,8 +1,66 @@
 <?php
 
 require_once('utils/bdd.php');
+require_once('controllers/upload.php');
 
-
-function uploadFile(){
+function uploadFileuser($usermail){
     
+
+    global $bdd;
+    $response=$bdd->prepare("INSERT INTO `users`(mail_user) VALUES ($usermail)");
+    $response->execute();
+
+   $result=$response->fetchAll(PDO::FETCH_ASSOC);
+   echo "de adresse user de request:";
+   var_dump ($usermail);
+echo "</br>";
+
+     return $result;
+}
+
+function uploadFileFile($path,$filename){
+    
+
+    global $bdd;
+    $response=$bdd->prepare("INSERT INTO `file`(url_file, name_file ) VALUES ($path,$filename)");
+    $response->execute();
+
+   $result=$response->fetchAll(PDO::FETCH_ASSOC);
+echo "de request path et nom fichier:";
+   var_dump ($path,$filename);
+echo "</br>";
+
+
+     return $result;
+}
+function uploadFileDest($destinatairemail){
+    
+
+    global $bdd;
+    $response=$bdd->prepare("INSERT INTO `destinatraire`(mail_destinataire) VALUES ($destinatairemail)");
+    $response->execute();
+
+   $result=$response->fetchAll(PDO::FETCH_ASSOC);
+   echo "de request destinataire mail:";
+   var_dump ($destinatairemail);
+echo "</br>";
+
+
+     return $result;
+}
+
+function uploadFileSend($message,$date){
+    
+
+    global $bdd;
+    $response=$bdd->prepare("INSERT INTO `send`(`message`,date_send) VALUES ($message,$date)");
+    $response->execute();
+
+   $result=$response->fetchAll(PDO::FETCH_ASSOC);
+   echo "de request message et date mail:";
+   var_dump ($message,$date);
+   echo "</br>";
+
+
+     return $result;
 }
